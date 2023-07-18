654f7a1dfd9ec3b54497fbd94f497f25b97ac1e6
Louis <7jia1421@gmail.com>
2023-07-19 01:29:01 +0800

    Initialize

---
+++ src/main/java/com/example/hostelmanagementsystem/HelloApplication.java
@@ -0,0 +1,27 @@
+package com.example.hostelmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        BorderPane bp= new BorderPane();
+        Label title = new Label("Hello World");
+        bp.setCenter(title);
+        Scene scene = new Scene(bp, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

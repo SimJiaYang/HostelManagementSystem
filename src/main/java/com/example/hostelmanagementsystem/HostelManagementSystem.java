e43698855d8c4c621014db1172f283dbc4644d1f
Tan Shan Ke <b210157b@sc.edu.my>
2023-07-23 23:27:36 +0800

    Add UI

--- src/main/java/com/example/hostelmanagementsystem/HostelManagementSystem.java
+++ src/main/java/com/example/hostelmanagementsystem/HostelManagementSystem.java
@@ -1,29 +1,34 @@
 package com.example.hostelmanagementsystem;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HostelManagementSystem extends Application {
+
+    private static Stage stage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        BorderPane bp= new BorderPane();
-        Label title = new Label("Hello World");
-        bp.setCenter(title);
-        Scene scene = new Scene(bp, 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        stage = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Hostel Management System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stage.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
-        System.out.println(new Person());
-        System.out.println(new Lecturer());
-        System.out.println(new Student());
         launch();
     }
 }
